171af40d4aea35ff663191911b4aac4bb2518c73
Mikhail Belov <m.belov@3commas.io>
2022-05-22 11:55:28 +0300

    add axios test, add prismic request in getEdgeProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import { useFirstPrismicDocument } from "@prismicio/react";
+// import axios from "axios";
+import { client } from "../prismic";
 
 interface Props {
   text: string;
+  content: any;
 }
 
-const Index = ({ text }: Props) => {
+const Index = ({ text, content }: Props) => {
   const [document] = useFirstPrismicDocument();
-  console.log(document);
 
   return (
     <>
       <h1>You're running React on the Edge!</h1>
-      <h1>{document?.data?.title} - prismic</h1>
+      <h1>{document?.data?.title} - prismic (client request)</h1>
+      <h1>{content?.data?.description} - prismic (getEdgeProps request)</h1>
       <p>{text}</p>
+      <img src="/image/pexels.jpg" alt="pexels"/>
     </>
   );
 }
@@ -30,9 +34,17 @@ export async function getEdgeProps() {
 
   const text = "getEdgeProps, text " + minutes;
 
+  // TODO: not working axios in getEdgeProps
+  // const image = await axios.get(
+  //   'https://images.pexels.com/photos/11254131/pexels-photo-11254131.jpeg?cs=srgb&dl=pexels-summer-rune-11254131.jpg&fm=jpg&w=900&h=800'
+  // );
+
+  const content = await client.getByUID("main_page", "main_page");
+
   return {
     props: {
       text,
+      content
     } as Props,
   };
 }
